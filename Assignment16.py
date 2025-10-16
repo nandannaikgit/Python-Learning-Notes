1953b38b00334aa1a57c29e05289bd091e84556e
nandannaikgit <nandannaik415@gmail.com>
2025-10-16 12:12:00 +0530

    16/10/2025

---
+++ Assignment16.py
@@ -0,0 +1,22 @@
+#Homework
+"""1. Greet Function: Write a function greet() that takes no arguments and prints a greeting message."""
+
+# def greet(name="greet"):
+#     print(f"Hello my name is {name}")
+# greet()
+
+
+"""2.Parameterized Greet: Write a function greet_user() that takes a name as input and prints a custom greeting."""
+
+# def greet_user(name):
+#     user_name=input("Enter a name: ")
+#     print(f"My name is {name}")
+    
+# greet_user("nandan")
+     
+     
+"""Sum Function: Write a function add_numbers(a, b) that returns the sum of two numbers. Call this function with different values."""
+
+def add(a,b):
+    return a + b
+print(add(1,2),add(3,4))
