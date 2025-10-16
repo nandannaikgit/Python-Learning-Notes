6783a311237f79b4a0f413a1a3bb230834de60a9
nandannaikgit <nandannaik415@gmail.com>
2025-10-16 12:57:32 +0530

    16/10/2025

--- Assignment17.py
+++ Assignment17.py
@@ -8,11 +8,11 @@
 
 """2. Recursive Function: Write a recursive function that calculates the sum of the first n numbers."""
 
-def recursion(n):
+# def recursion(n):
     
-    if n == 0:
-        return n
-    else:
-        return n + recursion(n-1)
+#     if n == 0:
+#         return n
+#     else:
+#         return n + recursion(n-1)
     
-print(recursion(5))
+# print(recursion(5))
