d67e51c8378f34b4618d7bc2eed4e50f70684983
nandannaikgit <nandannaik415@gmail.com>
2025-10-16 12:29:11 +0530

    16/10/2025

---
+++ Assignment17.py
@@ -0,0 +1,9 @@
+#Homework
+
+"""1. Lambda Function: Write a lambda function that multiplies two numbers."""
+
+multi = lambda a, b : a * b
+print(multi(2,3))
+
+
+"""2. Recursive Function: Write a recursive function that calculates the sum of the first n numbers."""
