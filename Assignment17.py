14febcfdebaae089ceb2390fe8d9ddf294d0687a
nandannaikgit <nandannaik415@gmail.com>
2025-10-16 12:57:01 +0530

    16/10/2025

--- Assignment17.py
+++ Assignment17.py
@@ -2,8 +2,17 @@
 
 """1. Lambda Function: Write a lambda function that multiplies two numbers."""
 
-multi = lambda a, b : a * b
-print(multi(2,3))
+# multi = lambda a, b : a * b
+# print(multi(2,3))
 
 
 """2. Recursive Function: Write a recursive function that calculates the sum of the first n numbers."""
+
+def recursion(n):
+    
+    if n == 0:
+        return n
+    else:
+        return n + recursion(n-1)
+    
+print(recursion(5))
