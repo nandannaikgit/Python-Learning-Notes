a56a333b86f8424a7ccc21cc97ac27d77895001c
nandannaikgit <nandannaik415@gmail.com>
2025-11-11 12:17:25 +0530

    11/11/2025

--- Day23.py
+++ Day23.py
@@ -17,24 +17,42 @@
   """
   
 
-class Mobile:
-    def __init__(self,brand,price):
-        self.brand = brand
-        self.price = price
+# class Mobile:
+#     def __init__(self,brand,price):
+#         self.brand = brand
+#         self.price = price
         
-    def display(self):
-        print(f"Mobile brand is {self.brand} and price is {self.price}")
+#     def display(self):
+#         print(f"Mobile brand is {self.brand} and price is {self.price}")
         
-M = Mobile("Motorola", 18000)
-V = Mobile("Vivo",15000)
+# M = Mobile("Motorola", 18000)
+# V = Mobile("Vivo",15000)
 
-M.display()
-V.display()
+# M.display()
+# V.display()
         
   
 """2.Method Definition:
 
- Create multiple objects of the Student class and call the method on each.
  Define a class Student with attributes name and marks.
- Write a method display_info() that prints the student's name and marks.
+Write a method display_info() that prints the student's name and marks.
+Create multiple objects of the Student class and call the method on each.
+
 """
+
+class Student:
+    
+    def __init__(self,name, marks):
+        self.name = name
+        self.marks = marks
+        
+    def display_info(self):
+        print(f"student name is {self.name}  and marks is {self.marks}")
+        
+a = Student("nandan",99)
+b = Student("praddep",88)
+c = Student("divakar",90)
+
+a.display_info()
+b.display_info()
+c.display_info()
