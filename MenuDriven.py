0c3d9996f8084341d8038a988a3fb914de5a78d7
nandannaikgit <nandannaik415@gmail.com>
2025-12-01 09:17:00 +0530

    1/12/2025

--- MenuDriven.py
+++ MenuDriven.py
@@ -10,35 +10,39 @@ def mul(a,b):
 def div(a,b):
     return a//b
 
+def display_menu():
+    print("-----  Simple Calculator ------")
+    print("1. Addition")
+    print("2. Subtraction")
+    print(". Multiplication")
+    print("4. Addition")
+    print("5. Exit ")
 
-print("-----  Simple Calculator ------")
-print("1. Addition")
-print("2. Subtraction")
-print(". Multiplication")
-print("4. Addition")
-print("5. Exit ")
 
-choice = int(input("Enter your choice: "))
+while(True):
+    display_menu()
+    choice = int(input("Enter your choice: "))
 
-if choice in {1,2,3,4}:
-    a = int(input("Enter First number: "))
-    b = int(input("Enter second number: "))
+    if choice in {1,2,3,4}:
+        a = int(input("Enter First number: "))
+        b = int(input("Enter second number: "))
 
-if choice == 1:
-    print("Result:", add(a,b))
-    
-elif choice == 2:
-    print("Result:", sub(a,b))
-    
-elif choice == 3:
-     print("Result:", mul(a,b))
-     
-elif choice == 4:
-     print("Result:", div(a,b))
-     
-elif choice == 5:
-    print("Quitting....")
-    
-else:
-    print("Invalid choice. Tyr again!!")
-    
+    if choice == 1:
+        print("Result:", add(a,b))
+        
+    elif choice == 2:
+        print("Result:", sub(a,b))
+        
+    elif choice == 3:
+        print("Result:", mul(a,b))
+        
+    elif choice == 4:
+        print("Result:", div(a,b))
+        
+    elif choice == 5:
+        print("Quitting....")
+        break
+        
+    else:
+        print("Invalid choice. Tyr again!!")
+        
