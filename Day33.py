e3c478aa657fa7da2937b09826266a028f4b3238
nandannaikgit <nandannaik415@gmail.com>
2025-12-05 08:03:20 +0530

    5/12/2025

--- Day33.py
+++ Day33.py
@@ -1,1 +1,14 @@
 # Errors and exception handling
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+try:
+    print(a/b)
+except Exception as e:
+    print(f"Error : {e}")
+else:
+    print("Yenu error barlilla")
+finally:
+    print("Program ended!")
